c3e0d810debae3ef9d6286e26f420f51f84e7acc
AmirBagheri <amiramb10@gmail.com>
2025-02-26 20:14:22 +0330

    ArticleCard style temporary

--- src/reptitives/ArticleCard.jsx
+++ src/reptitives/ArticleCard.jsx
@@ -2,11 +2,12 @@ import Style from './ArticleCard.module.css'
 function ArticleCard(props){
     return(
         <>
-        <div className={Style.card}></div>
+        <div className={Style.card}>
         <img src={props.imgUrl} alt="" />
         <h2>{props.title}</h2>
         <p className={Style.summary}>{props.summary}</p>
         <p>{props.ReadingTime}</p>
+        </div>
         </>
     )
 }
