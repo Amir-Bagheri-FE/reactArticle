8543dfc3a069534c83cac9fc95ecd518786e0c84
AmirBagheri <amiramb10@gmail.com>
2025-03-06 09:28:22 +0330

    Single Article page Styles +

--- src/pages/SingleArticle.jsx
+++ src/pages/SingleArticle.jsx
@@ -1,18 +1,26 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
-function SingleArticle(){
-    const Article=useParams()
-    let id=useLocation();
-    const [articleSingle,setArticleSingle]=useState({})
-    useEffect(()=>{
-        axios.get(`http://localhost:4000/data/${id.state.id}`).then(result=>setArticleSingle(result.data))
-    },[id])
-    return(
-     <>
-    <img src={articleSingle.imgUrl} alt="" />
-    <h1>{Article.title}</h1>
-    <p>{articleSingle.main}</p>
-    </>)
+import Style from "./SingleArticle.module.css";
+import Header from "../reptitives/Header";
+function SingleArticle() {
+  const Article = useParams();
+  let id = useLocation();
+  const [articleSingle, setArticleSingle] = useState({});
+  useEffect(() => {
+    axios
+      .get(`http://localhost:4000/data/${id.state.id}`)
+      .then((result) => setArticleSingle(result.data));
+  }, [id]);
+  return (
+    <>
+    <Header title={Article.title}/>
+      <div className={Style.featureimgCon}>
+        <img className={Style.featureimg} src={articleSingle.imgUrl} alt="" />
+      </div>
+      <h1 className={Style.Title}>{Article.title}</h1>
+      <p className={Style.main}>{articleSingle.main}</p>
+    </>
+  );
 }
 export default SingleArticle;
