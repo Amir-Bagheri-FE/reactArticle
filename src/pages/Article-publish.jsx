320b0da653d80d920a689d6aaa07423ae4d3d66c
AmirBagheri <amiramb10@gmail.com>
2025-03-10 21:18:29 +0330

    style + posting in publish page

--- src/pages/Article-publish.jsx
+++ src/pages/Article-publish.jsx
@@ -1,5 +1,7 @@
 import {useState } from "react";
 import Header from "../reptitives/Header";
+import axios from "axios";
+import './publish.css'
 function ArticleMaking(){
     const [newArticle,setNewArticle]=useState({})
     function handleInput(e){
@@ -8,22 +10,25 @@ function ArticleMaking(){
          [e.target.name]:e.target.value
         })); 
     }
+    function postArticle(){
+  axios.post('http://localhost:4000/data',newArticle)
+    }
     return(
 <>
 <Header title='publish Article'/>
-<h1>publish your Article here</h1>
-<div>
+<h1 className="pageTitle">publish your Article here</h1>
+<div className="formCon">
     <label htmlFor="title">Add the title for Article</label>
-    <input name='title' type='text' onChange={handleInput}/> <br/>
+    <input className="inputs" name='title' type='text' onChange={handleInput}/> <br/>
     <label htmlFor="summary">Add the short summary for Article</label>
-    <input name='summary' type='text' onChange={handleInput}/>  <br/>
+    <input className="inputs" name='summary' type='text' onChange={handleInput}/>  <br/>
     <label htmlFor="imgUrl">Add URL of feature image</label>
-    <input name='imgUrl' type='text' onChange={handleInput}/>  <br/>
+    <input className="inputs" name='imgUrl' type='text' onChange={handleInput}/>  <br/>
     <label htmlFor="ReadingTime">how much it takes to finish the Article</label>
-    <input name='ReadingTime' type='text' onChange={handleInput}/>  <br/>
+    <input className="inputs" name='ReadingTime' type='text' onChange={handleInput}/>  <br/>
     <label htmlFor="main">now write main part</label>
-    <input name='main' type='textarea' onChange={handleInput}/>
-    <input type="submit"/>
+    <input className="inputs" name='main' type='textarea' onChange={handleInput}/>
+    <input className="subButton" type="submit" onClick={postArticle}/>
 </div>
 </>)
 }
